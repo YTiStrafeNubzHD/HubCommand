889a84c53ada0a0bbbb029d6651f2f275e344168
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2020-12-14 17:01:58 +0100

    Update Main.php

--- src/HubCommand/Main.php
+++ src/HubCommand/Main.php
@@ -10,14 +10,6 @@ use pocketmine\Player;
 
 class Main extends PluginBase {
 
-    public function onEnable() : void {
-        $this->getLogger()->info(TextFormat::GREEN . "HubCommand has been enabled");
-    }
-
-    public function onDisable() : void {
-        $this->getLogger()->info(TextFormat::RED . "HubCommand has been disabled");
-    }
-
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
         $spawnLocation = $this->getServer()->getDefaultLevel()->getSpawnLocation();
         switch ($command->getName()) {
@@ -26,7 +18,7 @@ class Main extends PluginBase {
                 if ($sender instanceof Player) {
                     $sender->getPlayer()->teleport($spawnLocation);
                 } else {
-                    $sender->sendMessage(TextFormat::RED . "Please use this command in-game");
+                    $sender->sendMessage("Please use this command in-game");
                 }
                 break;
         }
